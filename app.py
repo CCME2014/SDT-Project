baed00761155b15ee63d14582301a3b8bde0fa7d
Tripleten <calvin.spivey@gmail.com>
2024-12-06 12:04:37 -0500

    'Debugging'

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ df_variables = df.clean_data(variable_choice)
 st.header('Distributions of Variables')
 
 # Create a histogram of the dependent variable (price)
-histo_price = df_price.create_visualization('price', kind='histogram', title='Histogram of Vehicle Price')
-st.write(histo_price)
+# histo_price = df_price.create_visualization('price', kind='histogram', title='Histogram of Vehicle Price')
+# st.write(histo_price)
 
 # Create a figure with two subplots
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(8, 6))
@@ -57,10 +57,10 @@ st.pyplot(fig)
 st.header('Exploring Price Trends')
 
 # Create the bar graph for the price mean by days listed
-bar_variables = df_dp.prepare_data_for_visualization('days_listed', 'price').create_visualization(x_col='days_listed', y_col='price_mean', 
-                 error_y='price_std', kind='scatter', title='Average Price by Days Listed with Standard Deviation and Point Size Representing Price Data Quanitity',
-                 size='marker_size')
-st.write(bar_variables)
+# bar_variables = df_dp.prepare_data_for_visualization('days_listed', 'price').create_visualization(x_col='days_listed', y_col='price_mean', 
+#                  error_y='price_std', kind='scatter', title='Average Price by Days Listed with Standard Deviation and Point Size Representing Price Data Quanitity',
+#                  size='marker_size')
+# st.write(bar_variables)
 
 # Calculate the correlation between days listed and price
 dp_correlation = df_dp['price'].corr(df_dp['days_listed'])
@@ -72,8 +72,9 @@ col1, col2 = st.columns(1,3)
 
 # Create the scatter plot between days listed and the odometer
 with col1:
-    scatter_date_listed_odometer = df_date_listed_odometer.create_visualization(x_col='days_listed', y_col='odometer', 
-                kind='scatter', title='Days Listed vs. Odometer')
+    # scatter_date_listed_odometer = df_date_listed_odometer.create_visualization(x_col='days_listed', y_col='odometer', 
+    #             kind='scatter', title='Days Listed vs. Odometer')
+    pass
 
 # Create the bar chart for days listed and the selected variable
 with col2:
@@ -85,45 +86,44 @@ with col2:
     ax.set_title(f'Days Listed by {option}')    
     st.pyplot(fig)
     
-# =============================================================================
-# # Create streamlit tabs for selecting the variables and displaying the regression model
-# with st.tabs():
-#     with st.tab("Variable Selection"):
-#         # Include a checkbox
-#         include_interactions = st.checkbox("Include Interaction Terms?", value=False)
-# 
-#         # Select independent variables
-#         selected_independent_variables = st.multiselect('Select Independent Variables for the OLS Regression Model:', 
-#                                                         independent_variables, default='days_listed')
-#         
-#         # Enable interaction selection only if at least 2 independent variables are selected
-#         if include_interactions and len(selected_independent_variables) >= 2:       
-#            selected_interaction_variables = st.multiselect('Select 2 Variables for an Interaction Analysis (Optional):', independent_variables,
-#                                                            max_selections=2)
-#         else:
-#             selected_interaction_variables = []  # Reset interaction selection if less than 2 independent variables chosen
-#   
-#         # Warning message
-#         if len(selected_interaction_variables) > 2:
-#             st.warning("Please select a maximum of 2 options for interaction analysis.")
-# 
-#     with st.tab("Model Results"):
-#         # Prepare data based on selections
-#         if selected_independent_variables:
-#             data = prepare_data(df_all.copy(), selected_independent_variables, selected_interaction_variables)
-#             
-#             # Add constant term
-#             X = sm.add_constant(data)
-#             y = df_all['price']
-#             
-#             # Fit the model
-#             model = sm.OLS(y, X).fit()
-#                 
-#             # Display results within Streamlit
-#             if model:
-#                 st.write(model.summary()) # Display model summary
-#             else:
-#                 st.write("Model fitting failed.")
-#         else:
-#             st.write("Please select your variables in the 'Variable Selection' tab.")
-# =============================================================================
+# Create streamlit tabs for selecting the variables and displaying the regression model
+with st.tabs():
+    with st.tab("Variable Selection"):
+        # Include a checkbox
+        include_interactions = st.checkbox("Include Interaction Terms?", value=False)
+
+        # Select independent variables
+        selected_independent_variables = st.multiselect('Select Independent Variables for the OLS Regression Model:', 
+                                                        independent_variables, default='days_listed')
+        
+        # Enable interaction selection only if at least 2 independent variables are selected
+        if include_interactions and len(selected_independent_variables) >= 2:       
+            selected_interaction_variables = st.multiselect('Select 2 Variables for an Interaction Analysis (Optional):', independent_variables,
+                                                            max_selections=2)
+        else:
+            selected_interaction_variables = []  # Reset interaction selection if less than 2 independent variables chosen
+  
+        # Warning message
+        if len(selected_interaction_variables) > 2:
+            st.warning("Please select a maximum of 2 options for interaction analysis.")
+
+    with st.tab("Model Results"):
+        # Prepare data based on selections
+        if selected_independent_variables:
+            data = prepare_data(df_all.copy(), selected_independent_variables, selected_interaction_variables)
+            
+            # Add constant term
+            X = sm.add_constant(data)
+            y = df_all['price']
+            
+            # Fit the model
+            model = sm.OLS(y, X).fit()
+                
+            # Display results within Streamlit
+            if model:
+                st.write(model.summary()) # Display model summary
+            else:
+                st.write("Model fitting failed.")
+        else:
+            st.write("Please select your variables in the 'Variable Selection' tab.")
+            
